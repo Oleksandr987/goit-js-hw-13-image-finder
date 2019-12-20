d1d8490ceca5352c08e2e3bc54be55869b04ed5b
Oleksandr Kryvenets <kralvi987@gmail.com>
2019-12-20 22:01:27 +0200

    added 1

---
+++ src/js/utils/refs.js
@@ -0,0 +1,6 @@
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  imagesList: document.querySelector('.gallery-js'),
+  loadMoreBtn: document.querySelector('button[data-action="load more"]'),
+  photoCardImage: document.querySelector('.photo-card'),
+};
